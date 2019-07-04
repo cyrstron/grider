5d783591d570fb1fa900b9653834768363195698
Kyryl Stronko <Kyryl.Stronko@logianalytics.com>
2019-07-04 18:38:15 +0300

    map-grid-tile

--- src/entities/tiles/map-grid-tile/map-grid-tile.ts
+++ src/entities/tiles/map-grid-tile/map-grid-tile.ts
@@ -1,3 +1,18 @@
+import {TileMercPoint} from '../../points/tile-merc-point';
+import { GridParams } from '../../grid-params';
+
 export class MapGridTile {
   
+    fromTileCoords(
+        {x, y}: grider.Point,
+        params: GridParams,
+        zoom: number,
+        tileWidth: number,
+        tileHeight: number,
+    ) {
+        const tilePoint = TileMercPoint.fromTile(x, y, tileWidth, tileHeight, zoom);
+        const delta = tilePoint.gridTileStartDelta(params);
+
+
+    }
 }
