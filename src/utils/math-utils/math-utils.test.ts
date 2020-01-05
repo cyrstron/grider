9ced2212c0de9ab22762945723e39fa2d5b500d9
Kyryl Stronko <kirstronko@gmail.com>
2020-01-05 21:00:57 +0200

    test

--- src/utils/math-utils/math-utils.test.ts
+++ src/utils/math-utils/math-utils.test.ts
@@ -1,7 +1,11 @@
 import {degToRad} from './';
 
 describe('math-utils:degToRag', () => {
-  it('converts degrees to radians', () => {
+  it('converts 180 degrees to PI', () => {
     expect(degToRad(180)).toBe(Math.PI);
+  });
+
+  it('converts 0 degrees to 0', () => {
+    expect(degToRad(0)).toBe(0);
   });
 });
